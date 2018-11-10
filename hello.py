88bd1750e589dd6f7eaf669d4d96e257fd694482
Himalay Mohanlal Joriwal <himalayj@amazon.com>
2018-11-09 18:34:49 -0800

    code save

--- hello.py
+++ hello.py
@@ -1,8 +1,32 @@
 # This prints out "John is 23 years old."
 name = "John"
 age = 23
-print("%s is %d years old." % (name, age))
+print ("%s is %d years old." % (name, age))
+
+print ("Hello World")
 
 # This prints out: A list: [1, 2, 3]
 mylist = [1,2,3]
-print("A list: %s" % mylist)
+print("A list: %s" % mylist, end=' ')
+
+y = "Those who know %s and those who %s" % (12, "HMJ")
+
+print(y)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end.  try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
---
+++ prompt.py
@@ -0,0 +1,7 @@
+age = input("How old are you? ")
+height = 5.5
+weight = 160
+print(age)
+
+# https://cito.github.io/blog/f-strings/
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
